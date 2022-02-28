75ac7f5347186edc36e7afcdc7bf8c5114d3b687
naamanjagga <namanjagga@cedcommerce.com>
2022-02-28 19:41:36 +0530

    jq alegibility

--- src/script.js
+++ src/script.js
@@ -1,22 +1,26 @@
-function myfunction() {
-	var age = document.getElementById('input1').value;
-	var salary = document.getElementById('input2').value;
+
+$(document).ready(function() {
+    $('#btn').on('click',function() {
+        var age = $('#input1').val()
+        var salary = $('#input2').val()
 
 	if (age >= 18) {
 		if (salary >=20000) {
 			if (salary >= 20000 && salary < 40000) {
-				document.getElementById('para1').innerHTML = 'loan upto 5 lakh rupees can be offered';
+				$('#para1').text('loan upto 5 lakh rupees can be offered')
 			} else if (salary >= 40000 && salary < 80000) {
-				document.getElementById('para1').innerHTML = 'loan upto 10 lakh rupees can be offered';
+				$('#para1').text('loan upto 10 lakh rupees can be offered')
 			} else if (salary >= 80000 && salary <= 200000) {
-				document.getElementById('para1').innerHTML = 'loan upto 20 lakh rupees can be offered';
+				$('#para1').text('loan upto 20 lakh rupees can be offered') 
 			} else {
-				document.getElementById('para1').innerHTML = 'loan upto 50 lakh rupees can be offered';
+				$('#para1').text('loan upto 50 lakh rupees can be offered')
 			}
 		} else {
-			document.getElementById('para1').innerHTML = 'below 20,000 salary no loan available';
+			$('#para1').text('below 20,000 salary no loan available')
 		}
 	} else {
-		document.getElementById('para1').innerHTML = 'below 18 year no loan facility available';
+		$('#para1').text('below 18 year no loan facility available');
 	}
-}
+
+    })
+})
